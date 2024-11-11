6c5c84ea948fc4cf08a2c45b1612fc9b6a9d0ef3
Fuma <leonardfuma26@gmail.com>
2024-11-11 17:17:56 +0200

    Consumer Models

--- Backend/src/middlewares/ErrorHandler.js
+++ Backend/src/middlewares/ErrorHandler.js
@@ -1,19 +1,20 @@
-const ApiError = require('../utils/ApiError'); // Adjust the path as needed
+const ApiError = require("../utils/ApiError")
 
-const ErrorHandling = (err, req, res, next) => {
-  // Initialize the error response object
-  const obj = {
-    statusCode: err instanceof ApiError ? err.statusCode || 500 : 400,
-    message: err.message || "An unexpected error occurred",
-    stack: process.env.NODE_ENV === 'development' ? err.stack : undefined,
-  };
+const ErrorHandling = (err,req,res,next)=>{
+    const obj ={}
+    if(err instanceof ApiError){
+                obj['statusCode'] = err.statusCode
+                obj['message'] = err.message
+                obj['stack'] = err.stack
 
-  // Set response status and return error details
-  res.status(obj.statusCode).json({
-    success: false,
-    message: obj.message,
-    ...(obj.stack && { stack: obj.stack }) // Include stack trace if in development mode
-  });
-};
 
-module.exports = ErrorHandling;
+    }else{
+        obj['statusCode'] = 400
+        obj['message'] = err.message
+        obj['stack'] = err.stack
+    }
+
+    res.status(obj.statusCode).json(obj)
+}
+
+module.exports =ErrorHandling
